c29965f1519e07f9630418be1389c0274d32ebef
FujitaMasa11 <mfujita.jp@gmail.com>
2025-05-18 15:31:04 +0900

    jQueryでの動作追加

--- Script.js
+++ Script.js
@@ -1,3 +1,16 @@
 $('#first_page').on('click', function() { 
     $('#first_page_wrapper').fadeOut(2000);
 });
+
+$('.Theme_Box').on('click', function() { 
+    let $answer=$(this).find(".Theme_input");
+    if($answer.hasClass("open")){
+      $answer.removeClass("open");
+    }else{
+      $answer.addClass("open");
+    }
+});
+
+$('.menu-icon').on('click', function() {
+    $('.menu').slideToggle(100);
+});
